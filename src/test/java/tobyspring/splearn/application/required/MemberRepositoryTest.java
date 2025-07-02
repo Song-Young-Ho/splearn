365fdd88683cf45a8a278b4c9a967cc9612d1eb1
dkf4929 <sng4929@naver.com>
2025-07-02 11:58:11 +0900

    test: memberRepository

---
+++ src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package tobyspring.splearn.application.required;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import tobyspring.splearn.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static tobyspring.splearn.domain.Member.register;
+import static tobyspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
+import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
+
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository repository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    void creteMember() {
+        Member member = register(createMemberRegisterRequest(), createPasswordEncoder());
+
+        assertThat(member.getId()).isNull();
+
+        repository.save(member);
+
+        assertThat(member.getId()).isNotNull();
+
+        entityManager.flush();
+    }
+}
