bb722a748d96f96277dc92b00067ce1c890bb849
dkf4929 <sng4929@naver.com>
2025-07-03 11:38:31 +0900

    test: valid & activate

---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -0,0 +1,59 @@
+package tobyspring.splearn.application.provided;
+
+import jakarta.persistence.EntityManager;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+import tobyspring.splearn.SplearnTestConfiguration;
+import tobyspring.splearn.domain.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Import(SplearnTestConfiguration.class)
+@Transactional
+public record MemberRegisterTest(MemberRegister memberRegister, EntityManager entityManager) {
+    @Test
+    void register() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThatThrownBy(() -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
+                .isInstanceOf(DuplicateEmailException.class);
+    }
+    
+    @Test
+    void activate() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+        entityManager.flush();
+        entityManager.clear();
+
+        member = memberRegister.activate(member.getId());
+        entityManager.flush();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void memberRegisterRequestFail() {
+        extracted(new MemberRegisterRequest("yh@splearn.app", "test", "longsecret"));
+        extracted(new MemberRegisterRequest("yh@splearn.app", "testesttestesttestesttestest", "longsecret"));
+        extracted(new MemberRegisterRequest("yhsplearn.app", "testesttestesttestesttestest", "longsecret"));
+    }
+
+    private void extracted(MemberRegisterRequest invalid) {
+        assertThatThrownBy(() -> memberRegister.register(invalid))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+}
